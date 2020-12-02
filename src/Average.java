99a1020433c018ed9d0586c9efa13ef53a505b8f
efsam <E.salimi75@ymail.com>
2020-12-02 18:18:01 +0330

    free commit

--- src/Average.java
+++ src/Average.java
@@ -4,6 +4,7 @@ public class Average {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Ave ave = (a, b) -> (a + b) / 2;
+
         System.out.println("Please enter two numbers...");
         System.out.println("Average is : " + ave.averageTwoNumbers(s.nextDouble(), s.nextDouble()));
     }
